9efc1b3b394338739b3b42bf1cd38549758cf6f7
JuanCG13 <58788407+JuanCG13@users.noreply.github.com>
2020-09-28 11:15:57 -0400

    cambios guardados en otra rama

--- templatedjango/apptemplate/admin.py
+++ templatedjango/apptemplate/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from templatedjango.apptemplate.models import *
 
 # Register your models here.
+
+admin.site.register(Compras)
+admin.site.register(Detalles_compras)
--- templatedjango/apptemplate/models.py
+++ templatedjango/apptemplate/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Compras(models.Model):
+    total_compra = models.CharField(max_length=400 ,blank=True, null=True)
+    fecha_compra = models.CharField(max_length=400 ,blank=True, null=True)
+
+    def __str__(self):
+        return "compra {}".format(self.fecha_compra)
+
+
+class Detalles_compras(models.Model):
+    
+    id_insumo = models.CharField(max_length=400 ,blank=True, null=True)
+    id_compra = models.CharField(max_length=400 ,blank=True, null=True)
+    cantidad = models.CharField(max_length=400 ,blank=True, null=True)
+    precio = models.CharField(max_length=400 ,blank=True, null=True)
+    subtotal = models.CharField(max_length=400 ,blank=True, null=True)
+
+    def __str__(self):
+        return "detalles {}".format(self.)
+
