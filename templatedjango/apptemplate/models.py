45e801fe264d4b406f0098b918659b43b6011413
JuanCG13 <58788407+JuanCG13@users.noreply.github.com>
2020-09-28 18:44:04 -0400

    terminando la bd

--- templatedjango/apptemplate/models.py
+++ templatedjango/apptemplate/models.py
@@ -59,17 +59,20 @@ class Detalles_insumos(models.Model):
 
 class Produccion(models.Model):
     
-    Fecha_estimada = models.CharField(max_length=400 ,blank=True, null=True)
-    estado = models.CharField(max_length=400 ,blank=True, null=True)
+    id_detalle_insumo = models.CharField(max_length=400 ,blank=True, null=True) # conectado a detalles insumos
+    fecha_produccion = models.CharField(max_length=400 ,blank=True, null=True)
+    estado_produccion = models.CharField(max_length=400 ,blank=True, null=True)
+    fecha_acabado = models.CharField(max_length=400 ,blank=True, null=True)
     
     
     def __str__(self):
         return "detalles {}".format(self.estado)
 
 class Detalles_Produccion(models.Model):
-    id_produccion = models.CharField(max_length=400,blank=True, null=True)
-    id_producto = models.CharField(max_length=400 ,blank=True, null=True)
-    producto = models.CharField(max_length=400 ,blank=True, null=True)
+    id_produccion = models.CharField(max_length=400,blank=True, null=True) # conectado a produccion
+    id_producto = models.CharField(max_length=400 ,blank=True, null=True) # conectado a producto
+    cantidad_detalle = models.CharField(max_length=400 ,blank=True, null=True)
+    cantidad_real_detalle = models.CharField(max_length=400 ,blank=True, null=True)
 
     def __str__(self):
         return "detalles {}".format(self.estado)
@@ -78,7 +81,7 @@ class Detalles_Produccion(models.Model):
 
 class Productos(models.Model):
     
-    Nombre_producto = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla compra
+    id_nombre_producto = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla compra
     id_categoria = models.CharField(max_length=400 ,blank=True, null=True)
     descripcion_producto = models.CharField(max_length=400 ,blank=True, null=True) # conectado a la tabla produccion
     cantidad_stock = models.CharField(max_length=400 ,blank=True, null=True)
@@ -95,18 +98,56 @@ class Categoria_productos(models.Model):
     def __str__(self):
         return "detalles {}".format(self.nombre_categoria)
 
+class Nombre_productos(models.Model):
+    
+    nombre_productos = models.CharField(max_length=400 ,blank=True, null=True)
+    
+    def __str__(self):
+        return "detalles {}".format(self.nombre_categoria)
+
 
 
 class Detalles_pedidos(models.Model):
     
-    Nombre_producto = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla compra
-    id_categoria = models.CharField(max_length=400 ,blank=True, null=True)
-    descripcion_producto = models.CharField(max_length=400 ,blank=True, null=True) # conectado a la tabla produccion
-    cantidad_stock = models.CharField(max_length=400 ,blank=True, null=True)
-    imagen = models.CharField(max_length=400 ,blank=True, null=True)
+    id_producto = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla producto
+    id_pedido = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
+    cantidad = models.CharField(max_length=400 ,blank=True, null=True)
+    subtotal_producto = models.CharField(max_length=400 ,blank=True, null=True) 
     precio = models.CharField(max_length=400 ,blank=True, null=True)
 
     def __str__(self):
         return "detalles {}".format(self.subtotal)
+
+
+class Pedidos(models.Model):
+    
+    id_cliente = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla producto
+    estado_pedido = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
+    fecha_pedido = models.CharField(max_length=400 ,blank=True, null=True)
+    fecha_entrega = models.CharField(max_length=400 ,blank=True, null=True) 
+    cantidad_total = models.CharField(max_length=400 ,blank=True, null=True)
+
+    def __str__(self):
+        return "detalles {}".format(self.subtotal)
+
+class Clientes(models.Model):
+    
+    nombre_cliente = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
+    direccion = models.CharField(max_length=400 ,blank=True, null=True)
+    telefono = models.CharField(max_length=400 ,blank=True, null=True) 
+
+    def __str__(self):
+        return "detalles {}".format(self.subtotal)
+
+class Ventas(models.Model):
+    
+    id_pedidos = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
+    id_clientes = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
+    fecha_venta = models.CharField(max_length=400 ,blank=True, null=True) 
+
+    def __str__(self):
+        return "detalles {}".format(self.subtotal)
+
+
 
 
