6856a483b1a444a59836339e261c520ecf83bc71
JuanCG13 <58788407+JuanCG13@users.noreply.github.com>
2020-12-22 09:07:23 -0300

    solucionando deploy de la vista de inicio

--- templatedjango/apptemplate/views.py
+++ templatedjango/apptemplate/views.py
@@ -525,7 +525,7 @@ def login(request):
             if user is not None:
                 dj_login(request, user)
                 # A backend authenticated the credentials
-                return redirect('/inicio')
+                return redirect('inicio/')
 
         return render(request,'login/login.html')
 
