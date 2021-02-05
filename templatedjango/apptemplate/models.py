65eeb068550d3d10e6a6c0bde8f897de70888828
JuanCG13 <58788407+JuanCG13@users.noreply.github.com>
2021-02-05 19:07:00 -0300

    modificando models de ventas y pedidos

--- templatedjango/apptemplate/models.py
+++ templatedjango/apptemplate/models.py
@@ -118,28 +118,18 @@ class Detalles_insumos(models.Model):
 # fin tablas producccion
 
 
+# tablas finales 
 class Detalles_pedidos(models.Model):
     
     id_producto = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla producto
     id_pedido = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
     cantidad = models.CharField(max_length=400 ,blank=True, null=True)
     subtotal_producto = models.CharField(max_length=400 ,blank=True, null=True) 
-    precio = models.CharField(max_length=400 ,blank=True, null=True)
+    # precio = models.CharField(max_length=400 ,blank=True, null=True)
 
     def __str__(self):
         return "detalles {}".format(self.subtotal)
 
-
-class Pedidos(models.Model):
-    
-    id_cliente = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla producto
-    estado_pedido = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
-    fecha_pedido = models.CharField(max_length=400 ,blank=True, null=True)
-    fecha_entrega = models.CharField(max_length=400 ,blank=True, null=True) 
-    cantidad_total = models.CharField(max_length=400 ,blank=True, null=True)
-
-    def __str__(self):
-        return "detalles {}".format(self.subtotal)
 
 class Clientes(models.Model):
     
@@ -152,13 +142,24 @@ class Clientes(models.Model):
 
 class Ventas(models.Model):
     
-    id_pedidos = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
-    id_clientes = models.CharField(max_length=400 ,blank=True, null=True)# conectado a la tabla pedido
+    
+    fecha_entrega = models.CharField(max_length=400 ,blank=True, null=True) 
     fecha_venta = models.CharField(max_length=400 ,blank=True, null=True) 
 
     def __str__(self):
         return "detalles {}".format(self.subtotal)
 
+
+class Pedidos(models.Model):
+    
+    id_cliente = models.ForeignKey(Productos, on_delete=models.CASCADE,blank=True, null=True) # conectado a la tabla producto
+    id_ventas = models.ForeignKey(Ventas, on_delete=models.CASCADE,blank=True, null=True) # conectado a la tabla pedido
+    estado_pedido = models.CharField(max_length=400 ,blank=True, null=True) # conectado a la tabla pedido
+    fecha_pedido = models.CharField(max_length=400 ,blank=True, null=True)
+    precio_total = models.CharField(max_length=400 ,blank=True, null=True)
+
+    def __str__(self):
+        return "detalles {}".format(self.subtotal)
 
 
 
